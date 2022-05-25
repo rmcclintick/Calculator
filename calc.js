271eb7c553cbc687b27dee260a62378326178e7e
Robert McClintick <robert.mcc3@gmail.com>
2022-05-25 11:27:34 -0400

    working on operators

--- calc.js
+++ calc.js
@@ -1,3 +1,8 @@
+//input values
+let num1;
+let num2;
+let operator;
+
 //output screen
 const output = document.querySelector('.input');
 
@@ -15,9 +20,48 @@ numberBtns.forEach(button => {
     })
 });
 
+//operator buttons
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        operate(button.textContent);
+    });
+});
+
+//equal button
+const equalBtn = document.querySelector('.equal-btn');
+equalBtn.addEventListener('click', evaluate);
+
 //clears screen and entry history
 function clearAll()
 {
     output.textContent = '';
     //will need to clear user entry array as well
+}
+
+//evaluate entry
+function evaluate()
+{
+    //broken
+    if (!num1 && !num2){}
+    else
+    {
+        if (!num2)
+    }
+}
+
+function operate(op)
+{
+    //incomplete
+    if (!output.textContent)
+        alert('empty');
+    else 
+    {
+        if (!num1)
+        {
+            num1 = Number(output.textContent);
+            operator = op;
+        } 
+    }
+    //alert(`num1:${num1} num2:${num2} op:${operator}`);
 }
